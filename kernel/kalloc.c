5fd96ddc2d6857870021f25c49f9aea5edf92383
LSD <1228515127@qq.com>
2020-12-24 18:04:37 +0800

    无

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -19,15 +19,17 @@ struct run {
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
-} kmem;
+	struct spinlock lock;
+	struct run *freelist;
+} kmem[NCPU];//一个CPU核心对应一个数组
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+	for(int i=0;i<NCPU;i++){
+		initlock(&kmem[i].lock, "kmem");
+	}
+	freerange(end, (void*)PHYSTOP);
 }
 
 void
@@ -35,6 +37,7 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
+  p += 4096; // XXX I can't get kernel.ld to place end beyond the last bss symbol.
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
 }
@@ -46,20 +49,23 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+	struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+	if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+		panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+	// Fill with junk to catch dangling refs.
+	memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+	r = (struct run*)pa;
+	//获得对应的CPU核心号
+	push_off();
+	int id=cpuid();
+	pop_off();
+	acquire(&kmem[id].lock);
+	r->next = kmem[id].freelist;
+	kmem[id].freelist = r;
+	release(&kmem[id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +74,33 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
-
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
-
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+	struct run *r;
+	push_off();
+	int id = cpuid();
+	pop_off();
+	acquire(&kmem[id].lock);
+	r = kmem[id].freelist;
+	if(r)
+		kmem[id].freelist = r->next;
+	release(&kmem[id].lock);
+	//当前CPU没有空闲内存块，从其他核心窃取一个
+	if(!r){
+		for(int i=0;i<NCPU;i++){
+			if(i==id){
+				continue;
+			}
+			acquire(&kmem[i].lock);
+			r = kmem[i].freelist;
+			if(r){
+				kmem[i].freelist = r->next;
+				release(&kmem[i].lock);
+				break;
+			}
+			release(&kmem[i].lock);
+		}
+	}
+	
+	if(r)
+		memset((char*)r, 5, PGSIZE); // fill with junk
+	return (void*)r;
 }
