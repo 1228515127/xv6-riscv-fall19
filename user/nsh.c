01367d3de1369a06283e5c1b01d6da57dfed5280
LSD <1228515127@qq.com>
2020-12-24 17:57:44 +0800

    无

---
+++ user/nsh.c
@@ -0,0 +1,129 @@
+
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
+
+int getcmd(char *buf, int nbuf);
+void getargv(char *cmd, char* argv[],int* argc);
+void runcmd(char*argv[],int argc);
+void execPipe(char*argv[],int argc); 
+ 
+#define MAXARGS 10
+#define MAXWORD 30
+#define MAXLINE 100
+ 
+int getcmd(char *buf, int nbuf){
+    fprintf(2, "@ ");
+    memset(buf, 0, nbuf);
+    gets(buf, nbuf);
+    if (buf[0] == 0) // EOF
+        return -1;
+    return 0;
+}
+char whitespace[] = " \t\r\n\v";
+char args[MAXARGS][MAXWORD];
+ 
+//*****************END  from sh.c ******************
+void getargv(char *cmd, char* argv[],int* argc){
+    // 让argv的每一个元素都指向args的每一行
+    for(int i=0;i<MAXARGS;i++){
+        argv[i]=&args[i][0];
+    }
+	
+    int i = 0; // 表示第i个word
+    int j = 0;//表示cmd的第i个字符
+    for (; cmd[j] != '\n' && cmd[j] != '\0'; j++)
+    {
+        // 每一轮循环都是找到输入的命令中的一个word，比如 echo hi ,就是先找到echo，再找到hi
+        // 让argv[i]分别指向他们的开头，并且将echo，hi后面的空格设为\0
+		
+        // 跳过之前的空格
+        while (strchr(whitespace,cmd[j])){
+            j++;
+        }
+		//argv[i]分别指向他们的开头
+        argv[i++]=cmd+j;
+        //跳过非空格
+        while (!strchr(whitespace,cmd[j])){
+            j++;
+        }
+        cmd[j]='\0';
+    }
+    argv[i]=0;
+    *argc=i;
+}
+ 
+void runcmd(char*argv[],int argc){
+	// 管道 | 和重定向 < > 前面都应该至少有一个word，所以从argv[1]开始
+    for(int i=1;i<argc;i++){
+        if(!strcmp(argv[i],"|")){
+            // 如果遇到 | 即pipe，至少说明后面还有一个命令要执行
+            execPipe(argv,argc);
+        }
+    }
+    
+    for(int i=1;i<argc;i++){
+        // 如果遇到 > ，说明需要执行输出重定向，首先需要关闭stdout
+        if(!strcmp(argv[i],">")){
+            close(1);
+            // 此时需要把输出重定向到后面给出的文件名对应的文件里
+            // 当然如果>是最后一个，那就会error，不过暂时先不考虑
+            open(argv[i+1],O_CREATE|O_WRONLY);
+            argv[i]=0;
+            // break;
+        }
+        if(!strcmp(argv[i],"<")){
+            // 如果遇到< ,需要执行输入重定向，关闭stdin
+            close(0);
+            open(argv[i+1],O_RDONLY);
+            argv[i]=0;
+            // break;
+        }
+    }
+    exec(argv[0], argv);
+}
+ 
+void execPipe(char*argv[],int argc){
+    int i=1;
+    // 首先找到命令中的"|",然后把他换成'\0'
+    // 从前到后，找到第一个就停止，后面都递归调用
+    for(;i<argc;i++){
+        if(!strcmp(argv[i],"|")){
+            argv[i]=0;
+            break;
+        }
+    }
+    int fd[2];
+    pipe(fd);
+    if(fork()==0){
+        // 子进程 执行左边的命令 把自己的标准输出关闭
+        close(1);
+        dup(fd[1]);//关闭标准输出，使用文件描述符fd，输出到管道fd
+        close(fd[0]);
+        close(fd[1]);
+        // exec(argv[0],argv);
+        runcmd(argv,i);
+    }else{
+        // 父进程 执行右边的命令 把自己的标准输入关闭
+        close(0);
+        dup(fd[0]);//关闭标准输入，使用文件描述符fd，从管道fd读入数据
+        close(fd[0]);
+        close(fd[1]);
+        // exec(argv[i+1],argv+i+1);
+        runcmd(argv+i+1,argc-i-1);
+    }
+}
+int main()
+{
+    char buf[MAXLINE];
+    while (getcmd(buf, sizeof(buf)) >= 0){
+        if (fork() == 0){
+            char* argv[MAXARGS];
+            int argc=-1;
+            getargv(buf, argv,&argc);
+            runcmd(argv,argc);
+        }
+        wait(0);
+    }
+    exit(0);
+}
